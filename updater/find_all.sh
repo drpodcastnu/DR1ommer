54b071a31ff231dbc38dd4598ee0ecffc371913d
Michael Bisbjerg <michael@mbwarez.dk>
2025-10-14 19:40:50 +0200

    Update find_all updater to produce new meta shows podcasts.json

--- updater/find_all.sh
+++ updater/find_all.sh
@@ -1,40 +1,116 @@
 #!/bin/bash
-replacements="
-s/^sara-og-monopolet-podcast$/sara-og-monopolet/g;
-s/^mads-monopolet-podcast$/sara-og-monopolet/g;
-s/^hjernekassen-paa-p1$/hjernekassen/g;
-s/^hjernekassen-pa-p1$/hjernekassen/g;
-s/^moerklagt-agent-samsam$/moerklagt/g;
-"
 
-curl 'https://api.dr.dk/radio/v2/series?limit=10000' --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0' -H 'Accept: application/json' -H 'Accept-Encoding: gzip, deflate, br' -H 'Referer: https://www.dr.dk/' -H 'x-apikey: 6Wkh8s98Afx1ZAaTT4FuWODTmvWGDPpR' > series.json
+set -euo pipefail
 
-echo -n > found_shows.txt
-echo -n '[' > new.json
-first=true
-IFS=$'\n'
-for show in $(jq -c '.items[] | { title: .title, url: .podcastUrl, urn: .id, slug: .slug}' series.json); do
-    TITLE=$(echo "$show" | jq -r '.title' -)
-    URL=$(echo "$show" | jq -r '.url' -)
-    if [ "$URL" = "null" ]; then
-      # Wasn't present for all shows
-      SLUG=$(echo "$show" | jq -r '.slug' - | sed 's/-[0-9]*$//')
-    else
-      SLUG=$(echo "$URL" | grep -o '[^/]*$')
-    fi
+# Fixes to normalize show slugs
+NORMALIZE_FILTER=$(cat <<'JQ'
+def replace_slug:
+  sub("sara-og-monopolet-podcast"; "sara-og-monopolet"; "g")
+  | sub("mads-monopolet-podcast"; "sara-og-monopolet"; "g")
+  | sub("hjernekassen-paa-p1"; "hjernekassen"; "g")
+  | sub("hjernekassen-pa-p1"; "hjernekassen"; "g")
+  | sub("moerklagt-agent-samsam"; "moerklagt"; "g");
 
-    SLUG=$(echo "$SLUG" | sed "$replacements")
-    echo "$TITLE ($SLUG)" >> found_shows.txt
-    if [ "$first" = "true" ]; then
-      first=false
-      echo >> new.json
-    else
-      echo "," >> new.json
-    fi
-    echo -n "$show" | jq -j "{ urn: .urn, slug: \"$SLUG\" }" >> new.json
-done
-unset IFS
-echo ']' >> new.json
+def derive_slug($url; $slug):
+  if $url != null and $url != "" then
+    ($url | split("/") | last | replace_slug)
+  elif $slug != null then
+    ($slug | sub("-[0-9]+$"; "") | replace_slug)
+  else
+    null
+  end;
 
-jq --slurpfile new new.json '.podcasts = $new[0]' ../ommer/src/main/resources/podcasts.json > podcasts.json
-mv podcasts.json ../ommer/src/main/resources/podcasts.json
+def derive_umbrella($umbrella):
+  if $umbrella != null then
+    (
+      ($umbrella.presentationUrl // "") as $url
+      | if $url != "" then
+          ($url | split("/") | last)
+        else
+          $umbrella.slug
+        end
+    )
+    | select(. != null)
+    | sub("-[0-9]+$"; "")
+    | replace_slug
+  else
+    null
+  end;
+
+.items |= map(
+  . + {
+    derivedSlug: derive_slug(.podcastUrl; .slug),
+    umbrellaSlug: derive_umbrella(.umbrella)
+  }
+)
+JQ
+)
+
+RAW_SERIES=updater/series.raw.json
+SERIES=updater/series.json
+
+# Download and fix the original podcasts feed
+curl 'https://api.dr.dk/radio/v2/series?limit=10000' \
+  --compressed \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0' \
+  -H 'Accept: application/json' \
+  -H 'Accept-Encoding: gzip, deflate, br' \
+  -H 'Referer: https://www.dr.dk/' \
+  -H 'x-apikey: 6Wkh8s98Afx1ZAaTT4FuWODTmvWGDPpR' \
+  > "$RAW_SERIES"
+
+jq "$NORMALIZE_FILTER" "$RAW_SERIES" > "$SERIES"
+
+tmp_dir=$(mktemp -d)
+trap 'rm -rf "$tmp_dir"' EXIT
+
+items_json="$tmp_dir/items.json"
+originals_json="$tmp_dir/originals.json"
+umbrellas_json="$tmp_dir/umbrellas.json"
+
+# Output shows for debugging
+jq '
+  .items[]
+  | select(.type == "Series")
+  | "\(.title) (\(.derivedSlug // ""))"
+' "$SERIES" > updater/found_shows.txt
+
+# Extract all shows 1:1
+jq '
+  [
+    .items[]
+    | select(.type == "Series" and .derivedSlug != null)
+    | {
+        slug: .derivedSlug,
+        urn: .id,
+        umbrella: .umbrellaSlug
+      }
+  ]
+' "$SERIES" > "$items_json"
+
+jq '[ .[] | { slug, urns: [.urn] } ]' "$items_json" > "$originals_json"
+
+# Find all shows that are part of an umbrella show (meta shows)
+jq '
+  [ .[] | select(.umbrella != null) ]
+  | group_by(.umbrella)
+  | map(select(length > 1))
+  | map({
+      slug: .[0].umbrella,
+      urns: (map(.urn) | unique | sort)
+    })
+' "$items_json" > "$umbrellas_json"
+
+# Merge all shows, meta and individual shows, into one final
+jq -s '
+  map(.[])
+  | group_by(.slug)
+  | map({
+      slug: .[0].slug,
+      urns: (map(.urns) | add | unique | sort)
+    })
+  | sort_by(.slug)
+' "$originals_json" "$umbrellas_json" > updater/new.json
+
+# Merge podcasts.template.json and new.json into podcasts.json
+jq --slurpfile podcasts updater/new.json '.podcasts = $podcasts[0]' podcasts.template.json > ommer/src/main/resources/podcasts.json
